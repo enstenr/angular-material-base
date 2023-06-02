25cb34bf6c27f14cc54c3d8a56363400a8926a8c
Rajesh subramanian <enstenr@gmail.com>
2023-06-02 20:59:15 +0530

    added 2 more components and menu file altered

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { Component1Component } from './component1/component1.component';
 import { Component2Component } from './component2/component2.component'
+import { Component2Component } from './component2/component3.component'
+import { Component2Component } from './component2/component4.component'
 const routes: Routes = [
   { path: 'load-comp1', component: Component1Component }, 
-  { path: 'load-comp2', component: Component2Component }, ];
+  { path: 'load-comp2', component: Component2Component }, 
+   { path: 'load-comp3', component: Component3Component },
+    { path: 'load-comp4', component: Component4Component },
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
